96a715c4bdbebb4c607c2e1a6e41a13e15273867
tushar@vivobook <tusharbansodkar@gmail.com>
2025-08-04 17:29:26 +0530

    minor changes

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,7 +1,7 @@
 const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
   return (
     <div
-      className="w-[60%] h-svh absolute left-0 top-16 justify-center rounded-tr-md rounded-br-md bg-[#0074B7]/90 transition-transform duration-300 ease-in-out"
+      className="w-[60%] h-svh absolute left-0 top-16 justify-center rounded-tr-md rounded-br-md bg-[#336CAE]/90 transition-transform duration-300 ease-in-out"
       style={{ transform: isOpen ? "translateX(0)" : "translateX(-110%)" }}
     >
       <ul className="space-y-8 w-[60%] text-center m-auto mt-10 a-auto">
